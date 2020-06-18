845001857d8589cdf02be1eb0dd4df64257af626
DruidWorks <40184288+druidworks@users.noreply.github.com>
2020-06-18 21:29:37 +0200

    Updated Homepage text

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }: {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hey, I am Kelly Truter, I have been a software developer for a 10+ years</p>
+        <p>Hey, I am Kelly Truter, I have been a software developer for a 10 years and counting.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
